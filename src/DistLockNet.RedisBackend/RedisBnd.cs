7147d19efb46ccaac5212635448203eec92b058f
officialhory <hory01@gmail.com>
2020-12-09 09:30:48 +0100

    project renaming and solution fixes

---
+++ src/DistLockNet.RedisBackend/RedisBnd.cs
@@ -0,0 +1,25 @@
+﻿using DistLockNet.Interfaces;
+using DistLockNet.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistLockNet.RedisBackend
+{
+    public class RedisBnd : ILockingBnd
+    {
+        public Task<LockingObject> GetAsync(string application, CancellationToken ct)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AddAsync(LockingObject lo, CancellationToken ct)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(LockingObject lo, CancellationToken ct)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
