2478618c09163ac1300fea5e57043013d9d32e49
Gergely Szentesi <gergely.szentesi@continental.com>
2020-12-09 15:01:14 +0100

    console ongoing

---
+++ src/DistLockTestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using DistLockNet;
+using DistLockNet.SqlBackend;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace DistLockTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"App-{Guid.NewGuid()} Started.");
+
+
+            var conf = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Uri.UnescapeDataString((new UriBuilder(Assembly.GetExecutingAssembly().CodeBase)).Path)))
+                .AddJsonFile("settings.json", optional: true, reloadOnChange: true).Build();
+
+            ILogger logger = new LoggerConfiguration()
+                .CreateLogger();
+
+            var bnd = new SqlBackend(conf, logger);
+
+            var locker = new Locker(conf, bnd);
+
+            locker.OnLockFail = (str) => logger.Debug($"Lock Fail: {str}");
+            locker.OnLockLost = (str) => logger.Debug($"Lock Lost: {str}");
+            locker.OnLockAcquired = (str) => logger.Debug($"Lock Acquired: {str}");
+        }
+    }
+}
