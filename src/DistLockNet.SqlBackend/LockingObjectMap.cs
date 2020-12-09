e14263c5e45a3e76a1169a24c2e8cad8564c12b0
officialhory <hory01@gmail.com>
2020-12-09 14:37:17 +0100

    entity map created

---
+++ src/DistLockNet.SqlBackend/LockingObjectMap.cs
@@ -0,0 +1,15 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace DistLockNet.SqlBackend
+{
+    public class LockingObjectMap : ClassMap<LockingObjectEntity>
+    {
+        public LockingObjectMap()
+        {
+            Table("distlocks");
+            Id(x => x.AppId);
+            Map(x => x.LockerId);
+            Map(x => x.Seed);
+        }
+    }
+}
