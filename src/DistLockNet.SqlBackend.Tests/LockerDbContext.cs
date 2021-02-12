dc58c2156eeecc025523b6965ffbfafdbcdaa506
Andor Auber (uic94595) <andor.auber-EXT@continental-corporation.com>
2021-02-12 16:15:07 +0100

    fixed tests

---
+++ src/DistLockNet.SqlBackend.Tests/LockerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DistLockNet.SqlBackend.Tests
+{
+    public class LockerDbContext : DbContext
+    {
+        private readonly IDataProviderSelector _dataProviderSelector;
+
+        public DbSet<LockingObjectEntity> LockingObjects { get; set; }
+
+        public LockerDbContext(IDataProviderSelector dataProviderSelector)
+        {
+            _dataProviderSelector = dataProviderSelector;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            _dataProviderSelector.UseSql(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LockingObjectEntity>();
+        }
+    }
+}
