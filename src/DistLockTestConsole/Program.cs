be899851703091f2ff7949cfbc89c61ce159e22f
Gergely Szentesi <gergely.szentesi@continental.com>
2021-03-24 08:56:49 +0100

    couchdb version added

--- src/DistLockTestConsole/Program.cs
+++ src/DistLockTestConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DistLockNet;
-using DistLockNet.SqlBackend;
+using DistLockNet.CouchBackend;
+using DistLockNet.CouchBackend.Model;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using System;
@@ -23,9 +24,19 @@ namespace DistLockTestConsole
                 .WriteTo.Console()
                 .CreateLogger();
 
-            var dps = new DataProviderSelector(conf);
-            var cf = new DbContextFactory(dps);
-            var bnd = new SqlBackend(cf, logger);
+            //var dps = new DataProviderSelector(conf);
+            //var cf = new DbContextFactory(dps);
+            //var bnd = new SqlBackend(cf, logger);
+
+
+            var bnd = new CouchBackend(new CouchConfig
+            {
+                DbName = "lock",
+                DoAuth = true,
+                Password = "admin",
+                Url = "http://localhost:5984",
+                UserName = "admin"
+            }, logger);
 
             var locker = new Locker(conf, bnd, logger)
             {
@@ -47,6 +58,15 @@ namespace DistLockTestConsole
 
             Console.ReadLine();
 
+        }
+
+        public class CouchConfig : ICouchConfig
+        {
+            public string Url { get; set; }
+            public string DbName { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public bool DoAuth { get; set; }
         }
     }
 }
